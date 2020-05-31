2ef406eaa34d9c947281d7502cccea5f1e37fd8d
gsatnad <gdantas1@mail.valenciacollege.edu>
2020-05-31 12:31:45 -0400

    base for the beginning

--- HangMan.py
+++ HangMan.py
@@ -1,6 +1,3 @@
-# This is the hangman game
-# Project started in May/30th/2020
-#
 import random
 def part6():
     print(' ________')
@@ -70,8 +67,10 @@ def part0():
     print(' |')
     print(' |')
     print(' |')
-    print('___________________')
-#str1 = input('Digite uma palavra: ')
+    print('___________________   => FRUIT', end='')
+
+#def checkGuess
+
 frutas = ['apple', 'grape', 'papaya', 'strawberry', 'mango', 'blueberry', 'watermelon', 'lemon', 'orange', 'cherry']
 # randint will generate a random number from 0 to 9
 selection = frutas[random.randint(0,9)]
@@ -79,37 +78,14 @@ print('resultado : ', selection)
 size = len(selection)
 y = ""
 for x in range(size):
-    y+= "_ "
+    y += "_*_   "
+part0()
+print('(',size, 'letters ) ', end='')
 print(y)
-print('\n\nIt has ',size,' letters')
-amt=5
-status = 0
-print ("U start with 5 chances")
-while status!=1:
-    z= input('digite uma letra: ')
-    if len(z)>1:
-        print ("EU DISSE 1 LETRA!! Repete aí")
-    if len(z)==1 :
-        found = selection.find(z)
-        if found == -1:
-            amt= amt-1
-            print((amt),'attempts remaining')
-        if found == 0:
-            y= y[1:]
-            new = selection[0] +y
-            y=new
-            print(y)
-        if found > 0:
-            print("good job")
-            print(selection.find(z))
-            dub = found*2
-            y = y[:dub] + selection[found] + y[dub + 1:]
-            print(y)
+Totalchances = 6
+gameOver = False
+userError = 0
 
-    if len(z)==0:
-        print("U ARE NOT FUN")
-    if amt==0:
-        print("WEAK")
-        status =1
-print('test')
+#while (gameover != true  and usererror < 6 ):
+ #   usertry = input('guess a letter: ')
 
